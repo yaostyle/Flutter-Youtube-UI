ac864a6e435c7299c2c7f931cfead965083d2d69
chsu <yaostyle@gmail.com>
2018-12-25 02:37:24 -0800

    Added library detail page.

--- lib/video_list.dart
+++ lib/video_list.dart
@@ -5,31 +5,35 @@ import 'package:flutter_youtube_ui/screens/video_detail.dart';
 class VideoList extends StatelessWidget {
   final List<YoutubeModel> listData;
   final bool isMiniList;
+  final bool isHorizontalList;
 
-  const VideoList({Key key, this.listData, this.isMiniList = false})
+  const VideoList(
+      {Key key,
+      this.listData,
+      this.isMiniList = false,
+      this.isHorizontalList = false})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final deviceOrientation = MediaQuery.of(context).orientation;
 
-    return ListView.separated(
-        shrinkWrap: true,
-        physics: ClampingScrollPhysics(),
+    if (isHorizontalList) {
+      return ListView.builder(
+        padding: const EdgeInsets.all(8.0),
+        scrollDirection: Axis.horizontal,
+        itemCount: listData.length,
         itemBuilder: (context, index) {
-          if (isMiniList || deviceOrientation == Orientation.landscape) {
-            return InkWell(
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => VideoDetail(
-                        detail: listData[index],
-                      ),
-                ));
-              },
-              child: _buildLandscapeList(context, index),
-            );
-          } else {
-            return InkWell(
+          return _buildHorizontalList(context, index);
+        },
+      );
+    } else {
+      return ListView.separated(
+          shrinkWrap: true,
+          physics: ClampingScrollPhysics(),
+          itemBuilder: (context, index) {
+            if (isMiniList || deviceOrientation == Orientation.landscape) {
+              return InkWell(
                 onTap: () {
                   Navigator.of(context).push(MaterialPageRoute(
                     builder: (context) => VideoDetail(
@@ -37,14 +41,26 @@ class VideoList extends StatelessWidget {
                         ),
                   ));
                 },
-                child: _buildPortraitList(context, index));
-          }
-        },
-        separatorBuilder: (context, index) => Divider(
-              height: 4.0,
-              color: Colors.grey,
-            ),
-        itemCount: listData.length);
+                child: _buildLandscapeList(context, index),
+              );
+            } else {
+              return InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => VideoDetail(
+                            detail: listData[index],
+                          ),
+                    ));
+                  },
+                  child: _buildPortraitList(context, index));
+            }
+          },
+          separatorBuilder: (context, index) => Divider(
+                height: 4.0,
+                color: Colors.grey,
+              ),
+          itemCount: listData.length);
+    }
   }
 
   Widget _buildLandscapeList(BuildContext context, int index) {
@@ -132,6 +148,54 @@ class VideoList extends StatelessWidget {
               child: Icon(Icons.more_vert)),
         ),
       ],
+    );
+  }
+
+  Widget _buildHorizontalList(BuildContext context, int index) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      width: 336.0 / 2.2,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(bottom: 4.0),
+            width: 336.0 / 2.2,
+            height: 188 / 2.2,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(listData[index].thumbNail)),
+            ),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 4.0),
+                      child: Text(
+                        listData[index].title,
+                        style: TextStyle(fontSize: 12.0),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Text(listData[index].channelTitle,
+                        style:
+                            TextStyle(fontSize: 12.0, color: Colors.grey[600])),
+                  ],
+                ),
+              ),
+              Icon(
+                Icons.more_vert,
+                size: 16.0,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
